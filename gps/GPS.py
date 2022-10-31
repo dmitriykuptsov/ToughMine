cb7e8a63ba29fb2ce8dd339036922a9d176826ba
Dmitriy Kuptsov <dmitriy.kuptsov@gmail.com>
2022-10-31 17:54:17 +0500

    Skeleton

---
+++ gps/GPS.py
@@ -0,0 +1,16 @@
+class GenericGPS():
+    def __init__(self):
+        pass
+    def open(self, config):
+        pass
+    def read(self):
+        pass
+
+class SerialGPS(GenericGPS):
+    def __init__(self):
+        pass
+    def open(self, config):
+        pass
+    def read(self):
+        pass
+
